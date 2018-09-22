8ba477f6af62667315d6661f2f82ac0b1eab504e
jrandiny <jrandiny2@gmail.com>
2018-09-22 15:19:30 +0700

    Add basic prompt

--- main.pas
+++ main.pas
@@ -3,14 +3,43 @@ program main;
 uses UDFA,UTipe,UData;
 
 {KAMUS}
+var
+  userFirst:boolean;
+  input:integer;
 
 {ALGORITMA}
 begin
-  loadDFA('dfa.txt');
+  writeln('  TUGAS BESAR IF2220  ');
+  writeln('    DFA TIK TAK TU    ');
+  writeln('----------------------');
+
+  writeln('Ingin siapa yang mulai dahulu ?');
+  writeln('1 untuk komputer (O)');
+  writeln('0 untuk kamu (X)');
+
+  repeat
+    write('Masukkan pilihan (1/O) = ');
+    readln(input);
+  until((input=1)or(input=0));
+
+  userFirst := (input = 0);
+
+  writeln('Loading DFA...');
+
+  if(userFirst)then
+  begin
+    loadDFA('dfa_user.txt');
+  end else
+  begin
+    loadDFA('dfa_computer.txt');
+  end;
+
+  writeln();
 
   while(true)do
   begin
-    inputAlphabet();
+    output();
+    inputAlphabet('Masukkan angka lokasi (1-9) = ');
     writeln('currState = ',currState);
   end;
 end.
