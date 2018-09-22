4007a178db4ba036d41b877a7c49273061153bd6
jrandiny <jrandiny2@gmail.com>
2018-09-22 14:11:12 +0700

    Add UTipe

---
+++ UTipe.pas
@@ -0,0 +1,27 @@
+unit UTipe;
+
+interface
+  const
+    NMAX = 100;
+    VALUNDEF = -99;
+
+
+  type TabInt = record
+    Isi : array[1..NMAX] of array[1..NMAX] of integer;
+    NKol : integer;
+    NBar : integer;
+  end;
+
+  type ArrString = record
+    Isi : array[1..NMAX] of string;
+    Neff : integer;
+  end;
+
+  type ArrInt = record
+    Isi : array[1..NMAX] of integer;
+    Neff : integer;
+  end;
+
+implementation
+
+end.
